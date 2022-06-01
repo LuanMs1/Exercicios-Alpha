b75e71e2c1f3314b5499484eb6893fa812fccea1
Luan Mesquita <luan.mesquita.sobrinho@gmail.com>
2022-06-01 17:55:54 -0300

    atividade 1 js

---
+++ JS/Módulo 5 js - Part1/index.js
@@ -0,0 +1,9 @@
+function addEvents(){
+    document.getElementById('helloWorld').addEventListener('click',hello);
+    return 0;
+}
+
+function hello(){
+    console.log("Olá console!");
+    return 0;
+}
