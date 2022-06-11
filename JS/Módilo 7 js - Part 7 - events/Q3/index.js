6ca22b399c8484a1e0ab667e3ddfb58470290d72
Luan <luan.mesquita.sobrinho@gmail.com>
2022-06-11 11:06:40 -0300

    dasda

---
+++ JS/Módilo 7 js - Part 7 - events/Q3/index.js
@@ -0,0 +1,29 @@
+//events
+
+function addEvents(){
+    document.querySelector('.submit').addEventListener('click',submitMessage);
+    document.querySelector('.camp').addEventListener('keyup',entSubmit);
+    document.querySelector('.erase').addEventListener('click',erase);
+}
+
+
+function submitMessage(){
+    const msg = document.querySelector('.camp').value;
+    const chat = document.querySelector('#msgs');
+    const msgBaloon = document.createElement('p');
+    msgBaloon.innerHTML = msg;
+    chat.innerHTML += msg + '\n';
+    chat.scrollTop = chat.scrollHeight;
+
+}
+
+function entSubmit(e){
+    if(e.key=='Enter'){
+        submitMessage();
+    }
+}
+
+function erase(){
+    const msg = document.querySelector('.camp');
+    msg.value = msg.value.slice(0,-msg.value.length);
+}
